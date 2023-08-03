0985ba09d835912055c8ad246174332c3d4e43e5
Will Hughes <will.hughes@zoom.us>
2023-08-02 22:22:10 -0400

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# zoom-participant-report-api
+# Zoom Webinar Participant Report Getter
+
+This is a sample app for a specific Zoom use case.
+
+scopes:
+- dashboard_webinars:read:admin
+- report:read:admin
+
+### Setup
+
+First install nodejs 18 LTS on your machine.
+
+
+```bash
+# clone the repo
+git clone https://github.com/Will4950/zoom-participant-report-api.git
+
+# Navigate into the cloned project directory
+cd zoom-participant-report-api
+
+# edit .env
+nano .env
+
+# Install required dependencies
+npm install 
+
+# Start the app
+npm start
+
+```
---
+++ zoom.js
@@ -0,0 +1,92 @@
+import axios from 'axios';
+
+const zoomAuth = 'https://zoom.us/oauth/';
+const zoomAPI = 'https://api.zoom.us/v2/';
+
+export async function getAccessToken() {
+	try {
+		let oauthToken = Buffer.from(
+			`${process.env.clientID}:${process.env.clientSecret}`
+		).toString('base64');
+
+		let res = await axios({
+			method: 'post',
+			url: `${zoomAuth}token?grant_type=account_credentials&account_id=${process.env.accountID}`,
+			headers: {Authorization: `Basic ${oauthToken}`}
+		});
+		return res.data.access_token;
+	} catch (e) {
+		return false;
+	}
+}
+
+export async function getPastWebinars(accessToken, webinars, token) {
+	let yesterdayDate = new Date(new Date().setDate(new Date().getDate() - 1))
+		.toJSON()
+		.slice(0, 10);
+	let currentDate = new Date().toJSON().slice(0, 10);
+	try {
+		let res = await axios({
+			method: 'get',
+			url: `${zoomAPI}/metrics/webinars`,
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+				'Content-Type': 'application/json'
+			},
+			params: {
+				type: 'past',
+				from: yesterdayDate,
+				to: currentDate,
+				page_size: 300,
+				next_page_token: token ? token : null
+			}
+		});
+		webinars = webinars.concat(res.data.webinars);
+		if (res.data.next_page_token) {
+			return await getPastWebinars(
+				accessToken,
+				webinars,
+				res.data.next_page_token
+			);
+		} else {
+			return webinars;
+		}
+	} catch (e) {
+		return false;
+	}
+}
+
+export async function getParticipantReport(
+	accessToken,
+	webinarID,
+	participants,
+	token
+) {
+	try {
+		let res = await axios({
+			method: 'get',
+			url: `${zoomAPI}report/webinars/${webinarID}/participants`,
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+				'Content-Type': 'application/json'
+			},
+			params: {
+				page_size: 300,
+				next_page_token: token ? token : null
+			}
+		});
+		participants = participants.concat(res.data.participants);
+		if (res.data.next_page_token) {
+			return await getParticipantReport(
+				accessToken,
+				webinarID,
+				participants,
+				res.data.next_page_token
+			);
+		} else {
+			return participants;
+		}
+	} catch (e) {
+		return false;
+	}
+}
